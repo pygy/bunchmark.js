6d4b324bc50a6fc5dbb8a84088acd7e308ed2062
Pierre-Yves Gérardy <py@py.gy>
2020-11-22 23:33:41 +0100

    refactor, add preliminary sandbox support

--- bunchmark.js
+++ bunchmark.js
@@ -1,10 +1,13 @@
 import {shuffled} from "./shuffle.js"
-import {compile} from "./compile.js"
-
-function findRepetition(sample) {
+import {compilePlain, compileIframe} from "./compile.js"
+export {compilePlain, compileIframe}
+export {stats} from "./stats.js"
+const delay = n => new Promise(f => setTimeout(f, n))
+// N is the baseline repetition count for a run
+async function findN(sample) {
 	let N = 1
 	for (;;) {
-		const t = sample(N)
+		const t = await sample(N)
 		if (t > 24) {
 			N = (N * 24 / t) | 0
 			break
@@ -15,48 +18,53 @@ function findRepetition(sample) {
 	return N
 }
 
-function runOne({sample, N, results}) {
-	// Uniform sampling in log space between 1 and 2
-	// given that the distribution still looks right-skewed in log space
-	// this seems reasonable.
-	const mul = 2**(1+Math.random()) / 2
+async function runOne({sample, N, results}) {
+	// When using a fixed number of repetitions we can end up with
+	// a striped timing distribution (depending on timer resolution),
+	// so we use a random multiplier to spread out the measurements.
+	// We sample uniformly in log space between 1 and 2.
+	// Given that we also plot in log space this seems reasonable.
+
+	const mul = 2**(1 + Math.random()) / 2
 	const reps = N * mul | 0
-	results.push(sample(reps) / reps)
+	results.push(await sample(reps) / reps)
 }
 
-function calibrate({tasks, beforeCalibration, onCalibrationTick}) {
-	return new Promise((f, r) => {
-		let i = 0
-		const reps = 16
-		tasks.forEach(t=>t.N = 0)
-		try {
-			beforeCalibration({reps})
-		} catch (e) {
-			return r(e)
+export async function calibrate({tasks, onCalibrationTick}) {
+	const reps = 16
+	tasks.forEach(t=>t.N = 0)
+	for (let i = 0; i < reps; i++) {
+		onCalibrationTick(i)
+		const sh = shuffled(tasks)
+		for (const i in sh) {
+			const t = sh[i]
+			t.N = Math.max(t.N, await findN(t.sample))
 		}
-		const runOnce  = () => {
-			onCalibrationTick(i)
-			try {
-				shuffled(tasks).forEach(t=>{t.N = Math.max(t.N, findRepetition(t.sample))})
-			} catch (e) {
-				return r(e)
-			}
-			if (++i === reps) {
-				f()
-			}
-			else setTimeout(runOnce)
-		}
-		runOnce()
-	})
+		await delay(0)
+	}
 }
 
-export function bunchmark({
+export async function run({tasks, reps, onRunTick}) {
+	let i = 0
+	while (i++ < reps()) {
+		onRunTick(i)
+		const sh = shuffled(tasks)
+		for (const i in sh) {
+			await runOne(sh[i])
+		}
+		
+		await delay(0)
+	}
+	return tasks.map(({name, N, results}) => ({name, N, results}))
+}
+
+
+export async function bunchmark({
 	tasks,
 	reps = 100,
 	prologue = "",
 	beforeEach = "",
 	afterEach = "",
-	deps = {},
 	displayEvery = 10,
 	onDisplayUpdate = () => {},
 	beforeCalibration = () => {},
@@ -65,49 +73,26 @@ export function bunchmark({
 	beforeRunning = () => {},
 	onRunTick = () => {},
 }) {
-	tasks = Object.entries(tasks).map(([name, v]) => ({
-		...v,
-		name,
-		results: [],
-		N: null,
-		sample: null
-	}))
-  
-	compile(tasks, {prologue, deps, beforeEach, afterEach})
-	return calibrate({tasks, beforeCalibration, onCalibrationTick, onCalibrationDone}).then(()=>new Promise((f, r) => {
-		try {
-			onCalibrationDone()
-			beforeRunning({reps, displayEvery})
-		} catch (e) {
-			return r(e)
-		}
-
-		let i = 0
-
-		const cycle = () => {
-			try{
-				onRunTick(i)
-				shuffled(tasks).forEach(runOne)
-			} catch (e) {
-				return r(e)
-			}
-			
-			// document.write(".")
-
-			if((onDisplayUpdate && (i % displayEvery === 0))) {
-				try {
-					onDisplayUpdate({reps, i, tasks: tasks.map(({name, N, results}) => ({name, N, results}))})
-				} catch (e) {
-					return r(e)
-				}		
-			}
-
-			if (++i === reps) {
-				f(tasks.map(({name, N, results}) => ({name, N, results})))
-			}
-			else setTimeout(cycle)
-			// if ((i) % 32 === 0)document.write("<br>")
-		}
-		setTimeout(cycle, 100)
-	}))
+	tasks = await compilePlain({
+		tasks: Object.entries(tasks).map(([name, v]) => ({
+			...v,
+			name
+		})),
+		prologue,
+		beforeEach,
+		afterEach
+	})
+	beforeCalibration({reps})
+	return calibrate({tasks, onCalibrationTick}).then(() => {
+		onCalibrationDone()
+		beforeRunning({reps, displayEvery})
+		return run({tasks, reps(){return reps}, onRunTick(i) {
+			onRunTick(i)
+			if (i % displayEvery === 0) onDisplayUpdate({
+				reps,
+				i, 
+				tasks: tasks.map(({name, N, results}) => ({name, N, results}))
+			})
+		}})
+	})
 }
---
+++ compile.js
@@ -0,0 +1,127 @@
+export {compileIframe, compilePlain}
+
+function stringify(x) {
+	for (const name in x) {
+		x = x[name]
+		if (typeof x === "string") return x
+		if (typeof x !== "function") throw new TypeError(`string or function expected for ${name}, got ${typeof x}`)
+		const source = x.toString()
+		return source.slice(source.indexOf("{") + 1, source.lastIndexOf("}"))
+		
+	}
+}
+
+const randStr = () => (Math.random() * 0x7fffffff | 0).toString(36)
+const uid = [randStr(),randStr(),randStr(),randStr()].join("_")
+const result_uid = `result_${uid}`
+const now_id = `now_${uid}`
+const hasPerf = typeof performance !== "undefined"
+const header = `
+const ${result_uid} = {};
+const ${now_id} = ${hasPerf ? "performance.now" : "process.hrtime.bigint"}
+`
+const plainFooter = (result_uid) => `
+return ${result_uid};
+`
+
+const N = "N_" + uid
+
+const bakeTask = ({run, before = "", after = "", name}, beforeEach, afterEach) => `
+${result_uid}[${JSON.stringify(name)}] = async function(${N}){
+	${stringify({beforeEach})};
+	${stringify({before})};
+	const t1 = ${now_id}()
+	while (${N}--) {
+${stringify({run})}
+	}
+	const t2 = ${now_id}()
+	${stringify({after})};
+	${stringify({afterEach})};
+	return Number(t2 - t1)${hasPerf ? "" : " / 1e6"}
+};
+	`
+
+
+export function bake({tasks, prologue = "", beforeEach = "", afterEach = "", footer}) {
+	return `
+${stringify({prologue})};
+${header};
+${tasks.map(task => bakeTask(task, beforeEach, afterEach)).join("")}
+${footer(result_uid)}
+	`
+}
+
+function compilePlain({tasks, prologue = "", beforeEach = "", afterEach = ""}) {
+	const samplers = new Function(bake({tasks, prologue, beforeEach, afterEach, footer: plainFooter}))()
+
+	return tasks.map(t=>({
+		...t,
+		results: [],
+		N: null,
+		sample: samplers[t.name]
+	}))
+}
+
+const isBrowser = typeof document !== "undefined"
+const {sanitizer, ifr} = isBrowser && {
+	sanitizer: document.createElement("div"),
+	ifr: (ifr => (ifr.style="display:none", ifr.sandbox = "allow-scripts", ifr))(document.createElement("iframe"))
+}
+
+// closes every tag so that the HTML can be prepended to the scripts.
+function sanitizeHTML(txt) {
+	sanitizer.innerHTML = txt
+	return sanitizer.innerHTML
+}
+
+// https://html.spec.whatwg.org/multipage/scripting.html#restrictions-for-contents-of-script-elements
+function sanitizeScript(txt) {
+	return txt.replace(/<(!--|script|\/script)/i, "<\\$1")
+}
+
+const iframeFooter = result_uid => `
+window.addEventListener("message", ({origin, data: {name, N}}) => {
+	if (origin === ${JSON.stringify(window.location.origin)}) {
+		${result_uid}[name](N).then(time => {
+			window.parent.postMessage({name, time, ${result_uid}: 1}}, ${JSON.stringify(window.location.origin)})
+		}).catch(({stack, message}) => {
+			window.parent.postMessage({name, stack, message, ${result_uid}: 1}}, ${JSON.stringify(window.location.origin)})
+		})
+	}
+})
+`
+
+function compileIframe({tasks, prologue = "", beforeEach = "", afterEach = "", html = ""}) {
+	ifr.srcdoc = `
+<!doctype html>
+<meta charset="utf8"><title>Bunchmark sandbox</title>
+${sanitizeHTML(html)}
+<script type=module>
+${sanitizeScript(bake({tasks, prologue, beforeEach, afterEach, footer: iframeFooter}))}
+</script>
+`
+	return new Promise(fulfill => {
+		ifr.onload = () => {
+			fulfill(tasks.map(t => ({
+				...t,
+				results: [],
+				N: null,
+				sample: N => {
+					return new Promise((fulfill_, reject_) => {
+						window.addEventListener("message", function handler({data}) {
+							window.removeEventListener("message", handler)
+							if (data[result_uid] === 1 && data.name === t.name) {
+								if ("time" in data) fulfill_(data.time)
+								else {
+									const {name, message, stack} = data
+									reject_({name, message, stack})
+								}
+							}
+						})
+						ifr.contentWindow.postMessage({name: t.name, N}, window.location.origin)
+					})
+				}
+			})))
+		}
+	})
+}
