f493b133931492b69ebdd5a83e87edc43a12c1ee
Pierre-Yves <py@py.gy>
2024-03-08 03:50:31 +0100

    core@1.0.0-pre-13; web@1.0.0-pre-14; typo

--- packages/core/src/run.js
+++ packages/core/src/run.js
@@ -134,7 +134,7 @@ function* runGenerator(options, state) {
             }
 
             for (const j in sh) {
-                console.log(Object.keys(sh[k]), sh[k].i)
+                console.log(Object.keys(sh[j]), sh[j].i)
                 yield { kind: "task", task: runOne(sh[j]), i: sh[j].i };
             }
         }
